8adf418ea8cb5b9d65e2330dbc3dd26f7541db36
Тимур Хабибов <Tim-him@mail.ru>
2022-12-10 19:16:24 +0500

    fix

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PG_CONNECTION } from '../constants';
-import { config } from 'dotenv';
 import * as useValue from './db.config';
 
-config();
 const dbProvider = {
   provide: PG_CONNECTION,
   useValue,
